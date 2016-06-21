37039a5495e5fe397669947d27f3b4ab37abfcec
kaiery <kaiery@qq.com>
2016-06-22 00:28:13 +0800

    修改部分Bug

---
+++ softfun_shell/src/main/java/com/softfun/utils/NetWorkUtils.java
@@ -0,0 +1,157 @@
+package com.softfun.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public final class NetWorkUtils {
+
+    private NetWorkUtils() {
+        throw new Error("Do not need instantiate!");
+    }
+
+
+
+    /**
+     * 检查网络是否可用
+     *
+     * @param paramContext
+     * @return
+     */
+    public static boolean checkEnable(Context paramContext) {
+        NetworkInfo localNetworkInfo = ((ConnectivityManager) paramContext.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+        if ((localNetworkInfo != null) && (localNetworkInfo.isAvailable()))
+            return true;
+        return false;
+    }
+
+    /**
+     * 将ip的整数形式转换成ip形式
+     *
+     * @param ipInt
+     * @return
+     */
+    public static String int2ip(int ipInt) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ipInt & 0xFF).append(".");
+        sb.append((ipInt >> 8) & 0xFF).append(".");
+        sb.append((ipInt >> 16) & 0xFF).append(".");
+        sb.append((ipInt >> 24) & 0xFF);
+        return sb.toString();
+    }
+
+    /**
+     * 获取当前ip地址
+     *
+     * @param context
+     * @return
+     */
+    public static String getLocalIpAddress(Context context) {
+        if(getCurrentNetType(context).equalsIgnoreCase("wifi")){
+            //wifi
+            try {
+                WifiManager wifiManager = (WifiManager) context
+                        .getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                int i = wifiInfo.getIpAddress();
+                return int2ip(i);
+            } catch (Exception ex) {
+                return " 获取IP出错鸟!!!!请保证是WIFI,或者请重新打开网络!\n" + ex.getMessage();
+            }
+        }else{
+            //移动流量
+            try {
+                for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                    NetworkInterface intf = en.nextElement();
+                    for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                        InetAddress inetAddress = enumIpAddr.nextElement();
+                        if (!inetAddress.isLoopbackAddress()) {
+                            return inetAddress.getHostAddress().toString();
+                        }
+                    }
+                }
+            } catch (SocketException ex) {
+            }
+            return null;
+        }
+    }
+
+
+    /**
+     * 判断网络类型
+     * @param context
+     * @return
+     */
+    public static String getCurrentNetType(Context context) {
+        String type = "";
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if (info == null) {
+            type = "null";
+        } else if (info.getType() == ConnectivityManager.TYPE_WIFI) {
+            type = "wifi";
+        } else if (info.getType() == ConnectivityManager.TYPE_MOBILE) {
+            int subType = info.getSubtype();
+            if (subType == TelephonyManager.NETWORK_TYPE_CDMA || subType == TelephonyManager.NETWORK_TYPE_GPRS
+                    || subType == TelephonyManager.NETWORK_TYPE_EDGE) {
+                type = "2g";
+            } else if (subType == TelephonyManager.NETWORK_TYPE_UMTS || subType == TelephonyManager.NETWORK_TYPE_HSDPA
+                    || subType == TelephonyManager.NETWORK_TYPE_EVDO_A || subType == TelephonyManager.NETWORK_TYPE_EVDO_0
+                    || subType == TelephonyManager.NETWORK_TYPE_EVDO_B) {
+                type = "3g";
+            } else if (subType == TelephonyManager.NETWORK_TYPE_LTE) {// LTE是3g到4g的过渡，是3.9G的全球标准
+                type = "4g";
+            }
+        }
+        return type;
+    }
+
+
+    public static String getProvider(Context context) {
+        String provider = "未知";
+        try {
+            TelephonyManager telephonyManager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+            String IMSI = telephonyManager.getSubscriberId();
+            Log.v("tag", "getProvider.IMSI:" + IMSI);
+            if (IMSI == null) {
+                if (TelephonyManager.SIM_STATE_READY == telephonyManager
+                        .getSimState()) {
+                    String operator = telephonyManager.getSimOperator();
+                    Log.v("tag", "getProvider.operator:" + operator);
+                    if (operator != null) {
+                        if (operator.equals("46000")
+                                || operator.equals("46002")
+                                || operator.equals("46007")) {
+                            provider = "中国移动";
+                        } else if (operator.equals("46001")) {
+                            provider = "中国联通";
+                        } else if (operator.equals("46003")) {
+                            provider = "中国电信";
+                        }
+                    }
+                }
+            } else {
+                if (IMSI.startsWith("46000") || IMSI.startsWith("46002")
+                        || IMSI.startsWith("46007")) {
+                    provider = "中国移动";
+                } else if (IMSI.startsWith("46001")) {
+                    provider = "中国联通";
+                } else if (IMSI.startsWith("46003")) {
+                    provider = "中国电信";
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return provider;
+    }
+}
